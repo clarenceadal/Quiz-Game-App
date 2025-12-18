e3ddd565068fe458a99262decb0ff7777742ac29
clarenceadal <clarenceeadaal@icloud.com>
2025-12-18 15:54:07 +0800

    Create QuizResult class to store quiz results
    Represents the result of a completed quiz, including score, total questions, and selected answers.

---
+++ quiz_result.dart
@@ -0,0 +1,13 @@
+class QuizResult {
+  final String category;
+  final int score;
+  final int totalQuestions;
+  final DateTime date;
+
+  QuizResult({
+    required this.category,
+    required this.score,
+    required this.totalQuestions,
+    required this.date,
+  });
+}
