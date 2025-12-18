ce448ab728c33409db256b907191b2344148f0cf
clarenceadal <clarenceeadaal@icloud.com>
2025-12-18 15:53:02 +0800

    Add QuizCategory class with properties
    Defines the quiz category data structure (e.g., id, title, description, number of questions).

---
+++ quiz_category.dart
@@ -0,0 +1,15 @@
+class QuizCategory {
+  final String id;
+  final String name;
+  final int questionCount;
+  final String difficulty;
+  final int highScore;
+
+  QuizCategory({
+    required this.id,
+    required this.name,
+    required this.questionCount,
+    required this.difficulty,
+    required this.highScore,
+  });
+}
