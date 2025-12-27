f150352455614ae5bd307c14bc092dd8c1145833
clarenceadal <clarenceeadaal@icloud.com>
2025-12-27 11:34:30 +0800

    Add correctAnswers field to QuizResult class

--- quiz_result.dart
+++ quiz_result.dart
@@ -1,16 +1,15 @@
 class QuizResult {
   final String category;
   final int score;
+  final int correctAnswers;
   final int totalQuestions;
   final DateTime date;
 
   QuizResult({
     required this.category,
     required this.score,
+    required this.correctAnswers,
     required this.totalQuestions,
     required this.date,
   });
 }
-
-
-
