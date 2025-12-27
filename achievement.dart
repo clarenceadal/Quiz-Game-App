66a530a00d6c78c727490a7b24888b5103c2f82d
clarenceadal <clarenceeadaal@icloud.com>
2025-12-27 11:31:36 +0800

    Add Achievement class with properties

---
+++ achievement.dart
@@ -0,0 +1,11 @@
+class Achievement {
+  final String id;
+  final String title;
+  final String description;
+
+  const Achievement({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+}
